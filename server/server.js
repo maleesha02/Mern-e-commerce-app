e3159b892d5187990683c38d650a2732c2f3e986
maleesha02 <weerasooriyamaleesha@gmail.com>
2026-06-13 23:05:15 +0530

    Added JWT authentication system

--- server/server.js
+++ server/server.js
@@ -2,17 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
-// connect database
+// Connect Database
 connectDB();
 
+// Initialize Express
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
